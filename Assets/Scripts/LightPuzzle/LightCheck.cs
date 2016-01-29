5db2552fbe8d662dcbce497d735f9408a0d2c5e5
o-s-s <matthewharrop.911@gmail.com>
2016-01-29 23:15:16 +0000

    Light puzzle script and test scene

---
+++ Assets/Scripts/LightPuzzle/LightCheck.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCheck : MonoBehaviour {
+	public GameObject previousMirror = null;
+	public GameObject nextMirror = null;
+	public double angleVariance = 10;
+
+	private double minAngle, maxAngle;
+
+	// Use this for initialization
+	void Start () {
+		if (nextMirror == null) {
+			// if this object is the target/light sink.
+			transform.LookAt (previousMirror.transform.position);
+
+			minAngle = transform.rotation.eulerAngles.z - angleVariance;
+			maxAngle = transform.rotation.eulerAngles.z + angleVariance;
+		} else if (previousMirror == null) {
+			// if this object is the light source.
+			transform.LookAt(nextMirror.transform.position);
+		}
+		else {
+			// if this object is not the light source or sink.
+			Vector2 toPrev = previousMirror.transform.position - transform.position;
+			Vector2 toNext = nextMirror.transform.position - transform.position;
+			double angle = Vector2.Angle(toPrev, toNext);
+
+			minAngle = angle - angleVariance;
+			maxAngle = angle + angleVariance;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	/*
+	 * check that this mirror is within tolerance.
+	 */
+	bool inTolerance() { return transform.rotation.eulerAngles.z >= minAngle && transform.rotation.eulerAngles.z <= maxAngle; }
+
+	/*
+	 * check that this and all prier mirrors are within tolerance angle to reflect light from source.
+	 */
+	bool inToleranceFromSource() {
+		if (previousMirror != null)
+			return inTolerance () && previousMirror.GetComponent<LightCheck> ().inToleranceFromSource();
+		else return inTolerance();
+	}
+}
