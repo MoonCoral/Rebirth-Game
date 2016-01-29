76d79fc40fd610e47f49fdd3d4b938678296621c
Jnicho2 <Jnicho@essex.ac.uk>
2016-01-29 22:47:49 +0000

    Added the switch prefab. Has public bool that turns true if anything is above it.

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	public bool triggered;
+
+	void Start () {
+		triggered = false;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		triggered = false;
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		triggered = true;
+	}
+}
