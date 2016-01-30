7a1bf22be93f5ddae41bb5f8641ce244cef847be
Jnicho2 <Jnicho@essex.ac.uk>
2016-01-30 21:51:54 +0000

    Added chest prefab

---
+++ Assets/Scripts/ChestScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChestScript : MonoBehaviour {
+
+	public Sprite open;
+	bool openned;
+
+	// Use this for initialization
+	void Start () {
+		openned = false;	
+	}
+	
+	// Update is called once per frame
+
+
+	void OnTriggerStay2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			if (Input.GetButtonUp ("Action")) {
+
+				if (!openned) {
+
+					openned = true;
+					this.gameObject.GetComponent<SpriteRenderer>().sprite = open;	
+
+				}
+				
+			}			
+		}
+	}
+}
