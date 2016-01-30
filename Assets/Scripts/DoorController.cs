a220304be03521170b2caa262f1b495457c29639
Icelightblue <pwleon@essex.ac.uk>
2016-01-30 23:27:58 +0000

    Door Prefab fixed and remade

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+	Animator anim;
+	public bool opened = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (opened)
+			anim.SetBool ("opened", true);
+		else
+			anim.SetBool ("opened", false);
+	}
+}
