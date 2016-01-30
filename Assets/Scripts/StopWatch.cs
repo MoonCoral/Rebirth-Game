c29018f6fc53afc3a35bbc949c39a8f32a5f1057
hbwigt <hbwigt@essex.ac.uk>
2016-01-30 21:48:18 +0000

    Updated Stopwatch -> Hourglass

--- Assets/Scripts/StopWatch.cs
+++ Assets/Scripts/StopWatch.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class StopWatch : MonoBehaviour
 {
-
+	public Temple temple;
     private GameObject stopwatch;
     private Image stopwatchImage;
     private GameObject shortHand;
@@ -35,42 +35,32 @@ public class StopWatch : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (time > 0.0)
-        {
-            if (time < 0.42)
-            {
+    void Update() {
+        if (time > 0.0) {
+            if (time < 0.42) {
                 shortHand.transform.Rotate(Vector3.forward * -0.5f);
                 longHand.transform.Rotate(Vector3.forward * 1);
-            }
-            else if (time < 0.84)
-            {
+            } else if (time < 0.84) {
                 shortHand.transform.Rotate(Vector3.forward * -1);
                 longHand.transform.Rotate(Vector3.forward * 2);
-            }
-            else if (time < 1.26)
-            {
+            } else if (time < 1.26) {
                 shortHand.transform.Rotate(Vector3.forward * -2);
                 longHand.transform.Rotate(Vector3.forward * 4);
-            }
-            else if (time < 1.68)
-            {
+            } else if (time < 1.68) {
                 shortHand.transform.Rotate(Vector3.forward * -4);
                 longHand.transform.Rotate(Vector3.forward * 8);
-            }
-            else if (time < 2.09)
-            {
+            } else if (time < 2.09) {
                 shortHand.transform.Rotate(Vector3.forward * -8);
                 longHand.transform.Rotate(Vector3.forward * 16);
-            }
-            else if (time < 2.50)
-            {
+            } else if (time < 2.50) {
                 shortHand.transform.Rotate(Vector3.forward * -16);
                 longHand.transform.Rotate(Vector3.forward * 32);
             }
             time -= Time.deltaTime;
-        }
+        } else {
+			shortHand.transform.eulerAngles = new Vector3(0, 0, temple.SecondsLeft()/4);
+			longHand.transform.eulerAngles = new Vector3(0, 0, (360*(temple.SecondsLeft()/120))+90);
+		}
     }
 
 }
