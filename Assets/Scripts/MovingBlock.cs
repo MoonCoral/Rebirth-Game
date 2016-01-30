4273ad64cfca165c4b6713288bac6faaee660438
Jnicho2 <Jnicho@essex.ac.uk>
2016-01-30 15:30:19 +0000

    Changes to player prefab

--- Assets/Scripts/MovingBlock.cs
+++ Assets/Scripts/MovingBlock.cs
@@ -6,9 +6,7 @@ public class MovingBlock : MonoBehaviour {
 
 	void OnTriggerStay2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
-			Debug.Log ("Please Move");
 			if (Input.GetButtonUp ("Action")) {
-				Debug.Log ("Please");
 
 				double x = getDirection(other.gameObject);
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,29 +5,21 @@ using UnityEngine.Networking;
 public class Player : MonoBehaviour
 {
 
-	
+	public Animator anim;
 
     public float speed = 1;
-    public bool playerControl = false;
-
-    public Sprite up;
-    public Sprite down;
-    public Sprite right;
-    public Sprite left;
-    public SpriteRenderer renderer;
+    public bool playerControl = false;	
 	public GameObject cam;
 
 	// Use this for initialization
 	void Awake ()
 	{
-        renderer = this.GetComponent<SpriteRenderer>();
-        renderer.sprite = down;
+		anim = this.GetComponent<Animator> ();
 		cam = GameObject.Find ("Main Camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		cam.transform.position = new Vector3 (transform.position.x, transform.position.y, -10);	
 	}
 
     void FixedUpdate()
@@ -39,20 +31,45 @@ public class Player : MonoBehaviour
         {
             temp.x += (speed * Input.GetAxis("Horizontal"));
             temp.y += (speed * Input.GetAxis("Vertical"));
+			transform.position = temp;
+						
+			if (Input.GetAxis("Horizontal") > 0) {
+				anim.SetBool("right", true);
+				anim.SetBool("left", false);
+				anim.SetBool("up", false);
+				anim.SetBool("down", false);
+			}
+            else if (Input.GetAxis("Horizontal") < 0) {
+				anim.SetBool("right", false);
+				anim.SetBool("left", true);
+				anim.SetBool("up", false);
+				anim.SetBool("down", false);
+			}
+            else if (Input.GetAxis("Vertical") > 0) {
+				anim.SetBool("right", false);
+				anim.SetBool("left", false);
+				anim.SetBool("up", true);
+				anim.SetBool("down", false);
+			}
+            else if (Input.GetAxis("Vertical") < 0) {
+				anim.SetBool("right", false);
+				anim.SetBool("left", false);
+				anim.SetBool("up", false);
+				anim.SetBool("down", true);
+			} else {
+				anim.SetBool("right", false);
+				anim.SetBool("left", false);
+				anim.SetBool("up", false);
+				anim.SetBool("down", false);
 
-            if (Input.GetAxis("Horizontal") > 0)
-                renderer.sprite = right;
-            else if (Input.GetAxis("Horizontal") < 0)
-                renderer.sprite = left;
-            else if (Input.GetAxis("Vertical") > 0)
-                renderer.sprite = up;
-            else if (Input.GetAxis("Vertical") < 0)
-                renderer.sprite = down;
+			}
 
-            transform.position = temp;
         }
 
-    }
+		cam.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y, -10);	
+
+		
+	}
 
     public void SetPlayerControl(bool toggle)
     {
