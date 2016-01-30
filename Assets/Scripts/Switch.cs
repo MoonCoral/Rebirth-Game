4236228386a4c4d10b4e47899a61b2b24d64f52e
Jnicho2 <Jnicho@essex.ac.uk>
2016-01-30 11:19:49 +0000

    Added prefabs for each tile

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -4,16 +4,23 @@ using System.Collections;
 public class Switch : MonoBehaviour {
 
 	public bool triggered;
+	public Sprite norm;
+	public Sprite pressed;
+	SpriteRenderer spriteRend;
 
 	void Start () {
 		triggered = false;
+		spriteRend = this.GetComponent<SpriteRenderer> ();
+		spriteRend.sprite = norm;
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
 		triggered = false;
+		spriteRend.sprite = norm;
 	}
 
 	void OnTriggerStay2D(Collider2D other) {
 		triggered = true;
+		spriteRend.sprite = pressed;
 	}
 }
