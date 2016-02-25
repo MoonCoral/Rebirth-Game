56f33cd72fe68ea80fe37601153796288596aefb
rdgain <rdgain@essex.ac.uk>
2016-02-25 16:25:45 +0000

    Most Puzzle 4 functionality implemented. TO DO: - clone player tether - add clone

---
+++ Assets/Scripts/PermaSwitchSolver.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PermaSwitchSolver : MonoBehaviour
+{
+
+    private ChestScript chest;
+    private PermaSwitch[] switches;
+
+    void Start ()
+	{
+	    chest = transform.parent.GetComponentInChildren<ChestScript>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        switches = transform.parent.GetComponentsInChildren<PermaSwitch>();
+
+        foreach (var s in switches)
+	    {
+            if (s.tag == "Switch" && !s.triggered)
+	        {
+	            return;
+	        }
+	    }
+
+	    chest.openned = true;
+	}
+}
