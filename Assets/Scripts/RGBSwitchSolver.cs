b63a91534c263503e78920753cc769e168705b43
rdgain <rdgain@essex.ac.uk>
2016-02-25 11:59:17 +0000

    RGB Puzzle (Puzzle 2) functionality completed. - clone and tiles to be added

---
+++ Assets/Scripts/RGBSwitchSolver.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RGBSwitchSolver : MonoBehaviour {
+	private RedSwitchScript switchR;
+	private GreenSwitchScript switchG;
+	private BlueSwitchScript switchB;
+    private ChestScript chest;
+
+	// Use this for initialization
+	void Start () {
+		switchR = GameObject.Find("switchR").GetComponent<RedSwitchScript> ();	
+		switchG = GameObject.Find("switchG").GetComponent<GreenSwitchScript> ();	
+		switchB = GameObject.Find("switchB").GetComponent<BlueSwitchScript> ();	
+        chest = transform.parent.GetComponentInChildren<ChestScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (switchB.triggered && switchR.triggered && switchB.triggered) {
+			chest.openned = true;
+		}
+	
+	}
+}
